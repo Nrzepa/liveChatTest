6e5cc00622b381ad82be0fa5ae843aec9570aec1
Nrzepa <119931277+Nrzepa@users.noreply.github.com>
2025-10-09 16:11:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,31 +32,26 @@ document.getElementById('connection-btn').addEventListener('click', function() {
             console.error('Error:', error);
         });
 
-      console.log('fetchData:', fetchData);
-    //   console.log('fetchData[access_token]:', fetchData[access_token]);
-    //   accessToken = fetchData[access_token];
-    //   console.log('accessToken:', accessToken);
-
-    //   fetch(
-    //     'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded',
-    //             'Accept': 'application/json',
-    //         },
-    //         body: `access_token=${accessToken}, lightning_out_app_id=1Usd100000004rdCAA`,
-    //     }
-    // )
-    //     .then((response) => {
-    //         console.log('Status:', response.status);
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //         console.log('Success:', data);
-    //     })
-    //     .catch((error) => {
-    //         console.error('Error:', error);
-    //         console.log(error.body);
-    //     });
+      fetch(
+        'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+            },
+            body: `access_token=${fetchData['access_token']}, lightning_out_app_id=1Usd100000004rdCAA`,
+        }
+    )
+        .then((response) => {
+            console.log('Status:', response.status);
+            return response.json();
+        })
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            console.log(error.body);
+        });
     });
