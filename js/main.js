6dc33f2a877531be8aa08418f03c8e69b41e6226
Nrzepa <119931277+Nrzepa@users.noreply.github.com>
2025-10-09 16:37:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,51 +9,60 @@ document.getElementById('connection-btn').addEventListener('click', function() {
       console.log('clientId: ' + clientId);
       console.log('clientSecret: ' + clientSecret);
 
-      fetch(
-        'https://beyond-connect-uat.my.salesforce.com/services/oauth2/token',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-        }
-    )
-        .then((response) => {
-            console.log('response: ' + JSON.stringify(response));
-            console.log('Status:', response.status);
-            return response.json();
-        })
-        .then((data) => {
-            fetchData = data;
-            accessToken = fetchData.access_token;
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 
-        setTimeout(
+      async function run() {
+        fetch(
+            'https://beyond-connect-uat.my.salesforce.com/services/oauth2/token',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+            }
+        )
+            .then((response) => {
+                console.log('response: ' + JSON.stringify(response));
+                console.log('Status:', response.status);
+                return response.json();
+            })
+            .then((data) => {
+                fetchData = data;
+                accessToken = fetchData.access_token;
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+            await sleep(3000);
+
             fetch(
-              'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
-              {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/x-www-form-urlencoded',
-                      'Accept': 'application/json',
-                  },
-                  body: `access_token=${accessToken}&lightning_out_app_id=1Usd100000004rdCAA`,
-              }
-          )
-              .then((response) => {
-                  console.log('Status:', response.status);
-                  return response.json();
-              })
-              .then((data) => {
-                  console.log('Success:', data);
-              })
-              .catch((error) => {
-                  console.error('Error:', error);
-                  console.log(error.body);
-              }), 3000);
+                'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Accept': 'application/json',
+                    },
+                    body: `access_token=${accessToken}&lightning_out_app_id=1Usd100000004rdCAA`,
+                }
+            )
+                .then((response) => {
+                    console.log('Status:', response.status);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    console.log(error.body);
+                });
+      }
+
+      run();
     });
