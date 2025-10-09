b35226bd1955930658f7e6fb62f36f11850cf6ac
Nrzepa <119931277+Nrzepa@users.noreply.github.com>
2025-10-09 15:36:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var clientId;
 var clientSecret;
+var accessToken;
 document.getElementById('connection-btn').addEventListener('click', function() {
       clientId = document.getElementById('client-id').value;
       clientSecret = document.getElementById('client-secret').value;
@@ -11,14 +12,35 @@ document.getElementById('connection-btn').addEventListener('click', function() {
         'https://beyond-connect-uat.my.salesforce.com/services/oauth2/token',
         {
             method: 'POST',
-            headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded',
-            },
             body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
         }
     )
         .then((response) => {
             console.log('response: ' + response);
+            console.log('Status:', response.status);
+            return response.json();
+        })
+        .then((data) => {
+            accessToken = data.access_token;
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            console.log(error.body);
+        });
+
+      fetch(
+        'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+            },
+            body: `access_token=${accessToken}, lightning_out_app_id=1Usd100000004rdCAA`,
+        }
+    )
+        .then((response) => {
             console.log('Status:', response.status);
             return response.json();
         })
@@ -29,27 +51,4 @@ document.getElementById('connection-btn').addEventListener('click', function() {
             console.error('Error:', error);
             console.log(error.body);
         });
-
-    //   fetch(
-    //     'https://beyond-connect-uat.salesforce.com/services/oauth2/lightningoutsingleaccess',
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded',
-    //             'Accept': 'application/json',
-    //         },
-    //         body: `access_token=00Dd1000006nBdB!AQEAQEi9IVSaodCcGJAWv2QLEv9gFLL3wyH1MYUCIJ.ooqWOopR5YYgsA3TvnD9K60fQsAooVyu1wcsq5ht9zEKbMDDrfs.L, lightning_out_app_id=1Usd100000004rdCAA`,
-    //     }
-    // )
-    //     .then((response) => {
-    //         console.log('Status:', response.status);
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //         console.log('Success:', data);
-    //     })
-    //     .catch((error) => {
-    //         console.error('Error:', error);
-    //         console.log(error.body);
-    //     });
     });
